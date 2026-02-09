9a5f633baba6496bbe3bac38c3d10b2183f6bb12
Rohit <rohitc129@gmail.com>
2026-02-09 17:27:24 +0530

    email template

--- app/email-template/page.tsx
+++ app/email-template/page.tsx
@@ -1,6 +1,15 @@
 import EmailTemplateComponent from "@/components/EmailTemplateComponent";
 import "./email-template.css";
 
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+}
+
 export default async function EmailTemplate() {
   return (
     <EmailTemplateComponent />
