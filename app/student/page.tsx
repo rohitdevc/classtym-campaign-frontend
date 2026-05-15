f34b62b09f3ff9a21f771a83b7aab37c6b7747ab
unknown <rohitc129@gmail.com>
2026-05-15 20:35:35 +0530

    revalidate to 0

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -16,7 +16,7 @@ const page_name = "Student";
 
 const [ meta_data, banner_data ] = await Promise.all([ getMetaData(headers, page_name), getBanner(headers, page_name) ]);
 
-export const revalidate = 600;
+export const revalidate = 0;
 
 export const metadata: Metadata = {
   title: meta_data.meta_title,
